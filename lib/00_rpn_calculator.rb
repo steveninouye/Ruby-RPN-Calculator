0cbfff1649b7d539fbf38fedf857aacd3301a7a9
Steven Inouye <steveninouye@msn.com>
2018-09-07 05:08:56 -1000

    completed RPN calculator

--- lib/00_rpn_calculator.rb
+++ lib/00_rpn_calculator.rb
@@ -2,39 +2,90 @@ class RPNCalculator
   # TODO: your code goes here!
   def initialize
     @store = []
-    @idx = 0
+    @push_count = 0
     @total = 0
   end
 
-  def push(num)
+  def push(num)    
+    @push_count += 1
+    if @push_count == 2 && @store.length > 2
+      @store = [@total, @store[-1]]
+      @total = 0
+    end
     @store.push(num)
-    @idx += 1
   end
 
   def plus
-    if @idx == 1
+    if @store.length == 0
+      raise "calculator is empty"
+    elsif @push_count == 0
       @total += @store[0]
     else
-      @total += @store[@idx-2...@idx].reduce(:+)
+      @total += @store[-2..-1].reduce(:+)
     end
-    @idx = 1
+    @push_count = 0
   end
 
   def minus
-    if @idx == 1
+    if @store.length == 0
+      raise "calculator is empty"
+    elsif @push_count == 0
       @total -= @store[0]
     else
-      @total += @store[@idx-2...@idx].reduce(:-)
+      @total += @store[-2..-1].reduce(:-)
     end
-    @idx = 1
+    @push_count = 0
+  end
+
+  def times
+    if @store.length == 0
+      raise "calculator is empty"
+    elsif @push_count == 0
+      @total *= @store[0].to_f
+    else
+      @total = @total + @store[-2..-1].reduce(:*)
+    end
+    @push_count = 0
+  end
+
+  def divide
+    if @store.length == 0
+      raise "calculator is empty"
+    elsif @push_count == 0
+      @total = @store[0].to_f / @total.to_f
+    else
+      @total += @store[-2..-1].reduce{|a,c| a.to_f / c.to_f}
+    end
+    @push_count = 0
+  end
+
+  def tokens(str)
+    str.split.map do |el|
+      if "*/+-".include? el
+        el.to_sym
+      else
+        el.to_i
+      end
+    end
+  end
+
+  def evaluate(str)
+    tokens(str).each do |el|
+      plus if el == :+
+      minus if el == :-
+      times if el == :*
+      divide if el == :/
+      push(el) if el == el.to_s.to_i
+    end
+    x = @total * 1
+    @store = []
+    @push_count = 0
+    @total = 0
+    x
   end
 
 
 
-
-
-
-  
 
   def value
     @total    
